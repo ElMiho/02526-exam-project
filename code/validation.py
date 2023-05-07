7ee1b274ebf1f9bac55f7bb3774e69da3f4eebce
ElMiho <mikael.hjermitslev@gmail.com>
2023-05-07 15:14:49 +0200

    It works with validation!

--- code/validation.py
+++ code/validation.py
@@ -6,6 +6,8 @@ import scipy
 import scipy.misc
 import scipy.cluster
 import binascii
+from sklearn.cluster import KMeans
+from PIL import Image
 
 # from sklearn import KMeans
 
@@ -13,7 +15,6 @@ def pixel2pred(x, attenuations):
     if x <= attenuations[0] + 1e-10:
         idx = 0
     elif x <= attenuations[1] + 1e-10:
-        print(x)
         idx = 1
     else:
         idx = 2
@@ -24,11 +25,12 @@ def return_circle(im):
     n,m = im.shape
     return_im = np.zeros((n,m))
     c = n//2
+    radius = c - 1
     for i in range(n):
         for j in range(m):
-            if ((i - c)**2 + (j - c)**2) <= c**2:   #Area to evaluate
+            if ((i - c)**2 + (j - c)**2) < radius**2:   #Area to evaluate
                 return_im[i,j] = im[i,j]
-    return np.nonzero(im)
+    return return_im
 
 def circle_to_im(vec):
     n = vec.shape[0]
@@ -37,31 +39,29 @@ def circle_to_im(vec):
     for i in range(n):
         for j in range(n):
             if ((i - c)**2 + (j - c)**2) <= c**2:   #Area to evaluate
-                return_im[i,j] = vec[i+j]
+                return_im[i][j] = vec[i+j]
     return return_im
 
 def predictions(recovered_image, original_image, attenuations):
-
     # Input: recovered image, original image, attenuations in the order wood, bismuth, steel
     # Returns a 3x3 confusion matrix and the predicted image based on most likelihood
     n,m = recovered_image.shape
     c = n//2
+    radius = c - 1
     predicted_image = np.zeros((n,m))
     
     confusion_matrix = np.zeros((5,5), dtype=object)
-    confusion_matrix[:,0] = ['-', 'Predicted', f'Wood: {attenuations[0]}', f'Steel: {attenuations[1]}', f'Bismuth: {attenuations[2]}']
-    confusion_matrix[0,:] = ['-', 'Actual', 'Wood', 'Steel', 'Bismuth']
+    confusion_matrix[:,0] = ['-', 'Predicted', 'Wood', 'Bismuth', 'Steel']
+    confusion_matrix[0,:] = ['-', 'Actual', 'Wood', 'Bismuth', 'Steel']
     confusion_matrix[:,1] = ['-','-','-','-','-']
     confusion_matrix[1,:] = ['-','-','-','-','-']
 
-    count = 0
     for i in range(n):
         for j in range(m):
-            if ((i - c)**2 + (j - c)**2) <= c**2:   #Area to evaluate
-                count += 1
+            if ((i - c)**2 + (j - c)**2) < radius**2:   #Area to evaluate
                 x = recovered_image[i,j]
-                #y = np.abs([x-attenuations[0],x-attenuations[1],x-attenuations[2]]) #Calculate max-likelyhood
-                idx = pixel2pred(x,attenuations)                          # Predicted class
+                y = np.abs([x-attenuations[0],x-attenuations[1],x-attenuations[2]]) #Calculate max-likelyhood
+                idx = np.argmin(y)                          # Predicted class
                 predicted_image[i,j] = attenuations[idx]    # Add value to image
                 if(idx==0):     # Predicted Wood 
                     if(original_image[i,j]==attenuations[0]):   # Actual value wood
@@ -85,7 +85,6 @@ def predictions(recovered_image, original_image, attenuations):
                     elif(original_image[i,j]==attenuations[2]): # Actual value steel
                         confusion_matrix[4,4] += 1
 
-    print("Count: {}".format(count))
     return predicted_image,confusion_matrix
 
 def downsized_unique_value(resized_im,attenuations):
@@ -93,14 +92,14 @@ def downsized_unique_value(resized_im,attenuations):
     # Returns: The most likely value of the resized image
     n,m = resized_im.shape
     c = n//2
+    radius = c-1
     im_unique_val = np.zeros((n,m))
     for i in range(n):
         for j in range(m):
-            if ((i - c)**2 + (j - c)**2) <= c**2:   #Area to evaluate
+            if ((i - c)**2 + (j - c)**2) < radius**2:   #Area to evaluate
                 x = resized_im[i,j]
-                idx = pixel2pred(x, attenuations)
-                # y = np.abs([x-attenuations[0],x-attenuations[1],x-attenuations[2]]) #Calculate max-likelyhood
-                # idx = np.argmin(y)                          # Predicted class
+                y = np.abs([x-attenuations[0],x-attenuations[1],x-attenuations[2]]) #Calculate max-likelyhood
+                idx = np.argmin(y)                          # Predicted class
                 im_unique_val[i,j] = attenuations[idx]    # Add value to image
     return im_unique_val
 
@@ -108,25 +107,24 @@ testImage = np.load("./testimage.npy")
 
 unique_values = sorted(np.unique(testImage))
 
-NUM_CLUSTERS = 3
+NUM_CLUSTERS = 4
 # kmeans = KMeans(n_clusters=NUM_CLUSTERS)
 
 downSamplingFactor = 100
 N = int(testImage.shape[1]/downSamplingFactor)
 resizedImage = ski.measure.block_reduce(testImage, block_size=downSamplingFactor, func=np.mean)
 
-import numpy as np
-from sklearn.cluster import KMeans
-from PIL import Image
-
 # Convert the image to a numpy array
-image_array = resizedImage
+# image_array = resizedImage
+
+print(f"Image shape: ", resizedImage.shape)
+image_array = return_circle(resizedImage)
 
 # Reshape the array to a 2D array of pixels
 pixel_values = image_array.reshape((-1, 1))
 
 # Perform k-means clustering with 4 clusters
-kmeans = KMeans(n_clusters=5, random_state=628).fit(pixel_values)
+kmeans = KMeans(n_clusters=NUM_CLUSTERS, random_state=628).fit(pixel_values)
 
 # Get the labels and centroid values for each pixel
 labels = kmeans.labels_
@@ -142,41 +140,9 @@ for i in range(kmeans.n_clusters):
 
 print("Cluster values: ", sorted(cluster_values))
 
-from sklearn.cluster import KMeans
-import numpy as np
-
-# Generate a random vector
-
-# vector = return_circle(resizedImage)
-
-# # Create a KMeans object with 3 clusters
-# kmeans = KMeans(n_clusters=3)
-
-# # Fit the data to the KMeans object
-# kmeans.fit(vector)
-
-# # Get the cluster labels for each data point
-# labels = kmeans.labels_
-
-# # Get the centroids of the clusters
-# centroids = kmeans.cluster_centers_
-
-# predicted_im = circle_to_im(labels)
-# plt.imshow(predicted_im)
-# plt.show()
-
-# # Fit the data to the KMeans object
-# kmeans.fit(testImage)
-
-# # Get the cluster labels for each data point
-# labels = kmeans.labels_
-
-# # Get the centroids of the clusters
-# centroids = kmeans.cluster_centers_
-
-# print(f"Labels: {labels}\n")
-# print(f"Centroids: {centroids}\n")
-
+predicted_im = labels.reshape(50,50)
+plt.imshow(predicted_im)
+plt.show()
 
 
 shape = testImage.shape
